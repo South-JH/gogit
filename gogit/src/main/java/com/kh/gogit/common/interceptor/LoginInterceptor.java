cdc22441dbba2d793081fd426c31bc252bba0299
(초롱) <crong9105@gmail.com>
2024-04-09 16:53:13 +0900

    4/9 commit

---
+++ gogit/src/main/java/com/kh/gogit/common/interceptor/LoginInterceptor.java
@@ -0,0 +1,29 @@
+package com.kh.gogit.common.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class LoginInterceptor extends HandlerInterceptorAdapter {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+		// true 리턴시 => 기존 요청 흐름대로 해당 Controller 제대로 실행
+		// false 리턴시 => Controller 실행되지 않음
+		
+		HttpSession session = request.getSession();
+		
+		if(session.getAttribute("loginUser") != null) {
+			// 현재 요청을 보낸 사람이 로그인 되어있을 경우 => Controller 실행
+			return true;
+		} else {
+			// 로그인이 되어있지 않을 경우 => Controller 실행 X
+			session.setAttribute("alertMsg", "로그인 후 이용가능한 서비스입니다.");
+			response.sendRedirect(request.getContextPath());
+			return false;
+		}
+	}
+	
+}
