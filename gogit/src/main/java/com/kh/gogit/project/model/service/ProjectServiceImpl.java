53eaa846612fe380c63a09527ac23900fa8fd964
(지민) <(hijimin039@gmail.com)>
2024-04-19 15:20:36 +0900

    .

--- gogit/src/main/java/com/kh/gogit/project/model/service/ProjectServiceImpl.java
+++ gogit/src/main/java/com/kh/gogit/project/model/service/ProjectServiceImpl.java
@@ -39,35 +39,6 @@ public class ProjectServiceImpl implements ProjectService{
 	public ArrayList<Project> selectList(PageInfo pi) {
 		return pDao.selectList(sqlSession, pi);
 	}
-	
-	
-	public void test1(Member loginUser) {
-		String url = "https://api.github.com/user/projects";
-		
-		RestTemplate restTemplate = new RestTemplate();
-		
-		HttpHeaders headers = new HttpHeaders();
-		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-		
-		headers.set("Authorization", "Bearer " + loginUser.getMemToken());
-		headers.set("Accept", "Accept: application/vnd.github+json");
-		
-		MultiValueMap<String, String> body = new LinkedMultiValueMap<String, String>();
-		body.add("name", "test");
-		body.add("body", "hi");
-		
-		HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<MultiValueMap<String,String>>(body, headers);
-		System.out.println("리퀘스트" + request);
-		
-		
-		ResponseEntity<String> response = restTemplate.postForEntity(url, request, String.class);
-		System.out.println("리스폰스"+ response);
-		
-		
-		if(response.getStatusCode() == HttpStatus.OK) {
-			System.out.println(response.getBody());
-		}	
-	}
 
 	@Override
 	public int insertProject(Project p) {
